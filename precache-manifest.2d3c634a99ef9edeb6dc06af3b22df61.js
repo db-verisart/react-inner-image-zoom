cf5b44717282001d082e2f8f11b3d81d014811b0
Lauren Ashpole <lauren@laurenashpole.com>
2020-08-04 23:57:06 -0400

    Updates

--- precache-manifest.2d3c634a99ef9edeb6dc06af3b22df61.js
+++ precache-manifest.2d3c634a99ef9edeb6dc06af3b22df61.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c65733e6fac4f12ce9e482852cfc4609",
+    "revision": "a2a1958602a1847d25a5889d6979785a",
     "url": "/react-inner-image-zoom/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-inner-image-zoom/static/css/2.345d6c2c.chunk.css"
   },
   {
-    "revision": "87274ebd899b418433f6",
-    "url": "/react-inner-image-zoom/static/css/main.161e2b31.chunk.css"
+    "revision": "8adf464f51d4194702c7",
+    "url": "/react-inner-image-zoom/static/css/main.2359f576.chunk.css"
   },
   {
     "revision": "dcf0fb691305e99e30e4",
     "url": "/react-inner-image-zoom/static/js/2.d96d0884.chunk.js"
   },
   {
-    "revision": "87274ebd899b418433f6",
-    "url": "/react-inner-image-zoom/static/js/main.832559e0.chunk.js"
+    "revision": "8adf464f51d4194702c7",
+    "url": "/react-inner-image-zoom/static/js/main.75fe9e76.chunk.js"
   },
   {
     "revision": "f3b3cd217e5ed5093bcf",
